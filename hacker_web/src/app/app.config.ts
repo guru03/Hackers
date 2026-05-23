ad04669ca3e4ea4a7a5d4979c030802a8a302343
Guru Shankar <guru03@outlook.com>
2026-05-23 22:54:20 +0530

    uptodate

--- hacker_web/src/app/app.config.ts
+++ hacker_web/src/app/app.config.ts
@@ -13,7 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideStore(appReducer),
-    provideStore(),
     provideStoreDevtools({ logOnly: !isDevMode() }),
 ]
 };
